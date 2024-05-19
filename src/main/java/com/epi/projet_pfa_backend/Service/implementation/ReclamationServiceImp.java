3a21fc7b942472f756752f8fe8373bd070243f04
Saharsaid <saharsaid03@gmail.com>
2024-05-19 22:27:55 +0100

    Réalisation de CRUD de Réclamation

--- src/main/java/com/epi/projet_pfa_backend/Service/implementation/ReclamationServiceImp.java
+++ src/main/java/com/epi/projet_pfa_backend/Service/implementation/ReclamationServiceImp.java
@@ -2,32 +2,51 @@ package com.epi.projet_pfa_backend.service.implementation;
 
 import com.epi.projet_pfa_backend.Service.ReclamationService;
 import com.epi.projet_pfa_backend.modele.Reclamation;
+import com.epi.projet_pfa_backend.repository.ReclamationRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@AllArgsConstructor
+@Service
 public class ReclamationServiceImp implements ReclamationService {
+    private final ReclamationRepository reclamationRepository;
     @Override
     public Reclamation createReclamation(Reclamation reclamation) {
-        return null;
+        return reclamationRepository.save(reclamation);
     }
 
     @Override
     public List<Reclamation> readReclamation() {
-        return List.of();
+        return reclamationRepository.findAll();
     }
 
     @Override
     public Reclamation updateReclamation(Long id, Reclamation reclamation) {
-        return null;
+
+        return reclamationRepository.findById(id)
+                .map(RE->{
+
+                    RE.setDescription(reclamation.getDescription());
+                    RE.setAdresse(reclamation.getAdresse());
+                    RE.setCitoyen(reclamation.getCitoyen());
+                    RE.setDateCreation(reclamation.getDateCreation());
+                    RE.setEtat(reclamation.getEtat());
+                    RE.setSousTypeProbleme(reclamation.getSousTypeProbleme());
+
+                    return reclamationRepository.save(RE);
+                }).orElseThrow(()->new RuntimeException("Réclamation non trouvé!!"));
     }
 
     @Override
     public String DeleteReclamation(Long id) {
-        return "";
+        reclamationRepository.deleteById(id);
+        return "Réclamation supprimé avec succées";
     }
 
     @Override
     public Reclamation getReclamationById(Long id) {
-        return null;
+        return reclamationRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Réclamation non trouvé!!"));
     }
 }
--- src/main/java/com/epi/projet_pfa_backend/controller/ReclamationController.java
+++ src/main/java/com/epi/projet_pfa_backend/controller/ReclamationController.java
@@ -1,12 +1,35 @@
 package com.epi.projet_pfa_backend.controller;
 
+import com.epi.projet_pfa_backend.Service.ReclamationService;
+import com.epi.projet_pfa_backend.modele.Actualite;
+import com.epi.projet_pfa_backend.modele.Reclamation;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Reclamtion")
 @AllArgsConstructor
 public class ReclamationController {
+
+    private final ReclamationService reclamationService;
+    @PostMapping("/createReclamation")
+    public Reclamation createReclamation(@RequestBody Reclamation reclamation) {
+        return reclamationService.createReclamation(reclamation);
+    }
+    @GetMapping("/getAllReclamation")
+    public List<Reclamation> getAllReclamation(){
+        return reclamationService.readReclamation();
+    }
+
+    @PutMapping("/updateReclamation/{id}")
+    public Reclamation updateReclamation(@PathVariable Long id,@RequestBody Reclamation reclamation){
+        return reclamationService.updateReclamation(id,reclamation);
+    }
+    @DeleteMapping("/deleteReclamation/{id}")
+    public String DeleteReclamation(@PathVariable Long id){
+        return reclamationService.DeleteReclamation(id);
+    }
 
 }
